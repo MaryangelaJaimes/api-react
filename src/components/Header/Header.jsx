b522f28bb56c258e6c1a17b0f56991d577c7ea9c
Maryangela Jaimes <maryangelajc.6@gmail.com>
2024-04-16 00:52:52 -0400

    cambio ruta img

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
+import silueta from "../../assets/silueta.svg";
 import "./Header.css";
 
 const Header = () => {
   return (
     <header className="banner d-flex justify-content-center mb-5">
-      <img className="banner-silhouette" src="/src/assets/silueta.svg" />
+      <img className="banner-silhouette" src={silueta} />
     </header>
   );
 };
