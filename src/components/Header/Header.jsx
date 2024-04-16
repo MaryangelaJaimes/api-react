45f85b062a04bc86070bc906d73a1d9e9fe654bd
Maryangela Jaimes <maryangelajc.6@gmail.com>
2024-04-16 00:42:18 -0400

    subida video readme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,9 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <>
-      <div className="banner d-flex justify-content-center mb-5">
-        <img className="banner-silhouette" src="/src/assets/silueta.svg" />
-      </div>
-    </>
+    <header className="banner d-flex justify-content-center mb-5">
+      <img className="banner-silhouette" src="/src/assets/silueta.svg" />
+    </header>
   );
 };
 
